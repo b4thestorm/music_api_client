b4206b89510ff1183b1e812cca997d4a3aa2b3e4
Arnold <arnoldsander@gmail.com>
2016-02-25 05:53:39 -0500

    music class refactor

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,27 +1,21 @@
 class Music
-	include HTTParty
-	base_uri 'https://api.discogs.com' 
- 	#default_params :api_key => ENV['DISCOGS_API_KEY'], :secret_key => ENV['DISCOGS_SECRET_KEY']
+#this model sets-up the query for the discogs api
+ 
+  include HTTParty
+  base_uri 'https://api.discogs.com'
+  format :json
+
+  HEADERS = {
+	'User-Agent' => 'Music_List/0.1', 
+	:key => ENV['DISCOGS_API_KEY'], 
+	:secret => ENV['DISCOGS_SECRET_KEY']})
+	} 
 
 
-	#.get{base_path}genre="hiphop"&year="1997"&per_page=50&page=1, headers: 
-	
-	def self.year_desired(age,dob)
-		age.to_i + dob.to_i
-	end
+  def self.query_songs(genre,year)
+  response = get("database/search?per_page=25&country=US&genre=Hip+Hop&year=2001", headers: Music::HEADERS )
+  end
 
-	 # def self.get_genre_by_year(genre,year)
-	 # 	get("database/search?genre=#{genre}&year=#{year}&per_page=50&page=1")
-	 # end  
-
-
-	 def self.get_artist(id)
-	 	response = get("/artists/#{id}/releases", headers: {"User-Agent" => 'Music_List/0.1'})
-	 end
-
-	 # def full_search
-	 # response = get("#{base_path}genre=#{genre}&year=#{year}&per_page=50&page=1&#{api_key}&#{secret}")  
-	 # end
 	
 	
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'music#index'
+  root to: 'music#index', via: :get
   resources :music, only: [:index,:new,:create]
 end
